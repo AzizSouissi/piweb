3928820e8be24235a146fbfb835b686c999c83cc
HaythemMekki <149381807+Haythem148@users.noreply.github.com>
2024-03-04 23:56:57 +0100

    haythem

---
+++ frontend/src/app/attendance-record-management/attendance-record-management.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AttendanceRecordManagementRoutingModule } from './attendance-record-management-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AttendanceRecordManagementRoutingModule
+  ]
+})
+export class AttendanceRecordManagementModule { }
