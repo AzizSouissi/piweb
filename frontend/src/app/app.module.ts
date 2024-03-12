50058321ebaa2aaf6e955fb53c6d9f1f35efcd7f
unknown <th7kingdom@gmail.com>
2024-03-12 22:52:09 +0100

    fixed some errors

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './modules/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RoleModule } from './modules/role/role.module';
 import { NavbarComponent } from './modules/navbar/navbar.component';
 import { SidebarComponent } from './modules/sidebar/sidebar.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
