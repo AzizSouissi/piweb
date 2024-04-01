fa274d0c2f4607883691fd948f8f884e20c80356
AzizSouissi <aziz.souissi01@gmail.com>
2024-04-01 23:16:55 +0100

    wipe

--- backend/nest/src/app.module.ts
+++ backend/nest/src/app.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HttpModule } from './infra/http/http.module';
-import { DatabaseModule } from './infra/database/database.module';
 import { PayrollModule } from './payroll/payroll.module';
 import { DeductionsModule } from './deductions/deductions.module';
 import { AllowancesModule } from './allowances/allowances.module';
 
 @Module({
-  imports: [
-    PayrollModule,
-    DeductionsModule,
-    AllowancesModule,
-    HttpModule,
-    DatabaseModule,
-  ],
+  imports: [PayrollModule, DeductionsModule, AllowancesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/nest/src/infra/http/params/recipient-id-param.ts
+++
@@ -1,6 +0,0 @@
-import { IsUUID } from 'class-validator';
-
-export class RecipientIdParam {
-  @IsUUID()
-  recipientId: string;
-}
