2a099276f4929d6de151cda96e4005ca0adab548
Haythem Mekki <haythem.mekki@esprit.tn>
2024-04-04 23:41:08 +0100

    added TaskManagement

--- frontend/src/app/core/models/user.ts
+++ frontend/src/app/core/models/user.ts
@@ -1,7 +1,7 @@
-import { AttendanceRecord } from './attendanceRecord';
+//import { AttendanceRecord } from './attendanceRecord';
 export class User {
   id!: string;
   firstname!: string;
   lastname!: string;
-  attendanceRecord!: AttendanceRecord[];
+  //attendanceRecord!: AttendanceRecord[];
 }
