ae88ade734730758cbd739cd116245736acc987f
nourmehdii <nour.mehdi@esprit.tn>
2024-04-04 14:10:32 +0100

    only task 2

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,14 +4,6 @@ import { HomeComponent } from './modules/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-
-  {
-    path: 'holiday',
-    loadChildren: () =>
-      import(
-        './modules/holiday-management/holiday-management.module'
-      ).then((m) => m.HolidayManagementModule),
-  },
   {
     path: 'task',
     loadChildren: () =>
