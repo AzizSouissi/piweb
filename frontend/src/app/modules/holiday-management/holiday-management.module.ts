2cbb17fc48a885ef4d471e87f54d0fb3593b2313
youssef chaabi <chaabiy214@gmail.com>
2024-04-24 20:09:53 +0100

    commit
    youssef

--- frontend/src/app/modules/holiday-management/holiday-management.module.ts
+++ frontend/src/app/modules/holiday-management/holiday-management.module.ts
@@ -4,27 +4,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListHolidayComponent } from './list-holiday/list-holiday.component';
 import { AddHolidayComponent } from './add-holiday/add-holiday.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { UpdateHolidayComponent } from './update-holiday/update-holiday.component';
+import { AgendaComponentComponent } from './agenda-component/agenda-component.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 const routes: Routes = [
   {path:'',component:ListHolidayComponent},
   {path:'AddHoliday',component:AddHolidayComponent},
-  {path:'UpdateHoliday/:id',component:UpdateHolidayComponent}
-
+  {path:'UpdateHoliday/:id',component:UpdateHolidayComponent},
+  {path:'agenda',component:AgendaComponentComponent}
   
 ]
 
 @NgModule({
   declarations: [
-    AddHolidayComponent,ListHolidayComponent, UpdateHolidayComponent
+    AddHolidayComponent,ListHolidayComponent, UpdateHolidayComponent, AgendaComponentComponent
   ],
   imports: [
     
     CommonModule,
     RouterModule.forChild(routes),
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+FullCalendarModule
   ],
   exports: [RouterModule],
 })
