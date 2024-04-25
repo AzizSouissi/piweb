57a93da52e9a9deb0cb416faefac439acc52da88
Mohamed El Hedi Hamdi <mohamedhedi1hamdi@gmail.com>
2024-04-25 14:01:44 +0200

    Update user.ts

--- frontend/src/app/core/models/user.ts
+++ frontend/src/app/core/models/user.ts
@@ -1,15 +1,20 @@
+import { Allowance } from './allowance';
 import { AttendanceRecord } from './attendanceRecord';
+import { Deduction } from './deduction';
+import { Payroll } from './payroll';
 import { Project } from './project';
 export class User {
   id!: string;
   firstname!: string;
   lastname!: string;
-  email!: string;
   attendanceRecord!: AttendanceRecord[];
   project!: Project[];
+  payrolls!: Payroll[];
+  allowances!: Allowance[];
+  deductions!: Deduction[];
   basicSalary!: number;
   offDays!: number;
-  familySituation!: string;
+  familySituation!: number;
   childrenNumber!: number;
   bankrib!: string;
   numCnss!: string;
