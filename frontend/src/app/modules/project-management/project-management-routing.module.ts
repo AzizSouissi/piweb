6cef8f32dded5375ce4f55db75cab525e9dcf1bc
Haythem Mekki <haythem.mekki@esprit.tn>
2024-04-28 22:42:25 +0100

    add-project

--- frontend/src/app/modules/project-management/project-management-routing.module.ts
+++ frontend/src/app/modules/project-management/project-management-routing.module.ts
@@ -3,8 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListProjectsComponent } from './ListProjects/ListProjects.component';
 import { AuthGuard } from '../../core/guards/auth-guard.guard';
 import { ProjectGuard } from '../../core/guards/project-guard.guard';
+import { AddProjectComponent } from './addProject/addProject.component';
 
-const routes: Routes = [{ path: '', component: ListProjectsComponent ,canActivate:[AuthGuard,ProjectGuard] }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListProjectsComponent,
+    canActivate: [AuthGuard, ProjectGuard],
+  },
+  { path: 'addProject', component: AddProjectComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
